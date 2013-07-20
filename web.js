b4bf4558a12743bfdba99a36074067a5de2406c2
Razvan L-M <razvan.lada@gmail.com>
2013-07-19 21:14:41 -0400

    Styled bitstarter

--- web.js
+++ web.js
@@ -6,9 +6,18 @@ buffer = fs.readFileSync("index.html");
 
 str = buffer.toString();
 
+app.use('/css',express.static(__dirname + '/bootstrap/css/'));
+app.use('/css',express.static(__dirname + '/font-awesome/css/'));
+app.use('/css',express.static(__dirname + '/bootstrap-social-buttons/css/'));
+
+app.use('/js',express.static(__dirname + '/bootstrap/js/'));
+app.use('/js',express.static(__dirname + '/jquery/js/'));
+
 app.get('/', function(request, response) {
   response.send(str);
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
